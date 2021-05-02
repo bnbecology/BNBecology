d32b626511952487cac8a8c95453b7f19c1eff7e
dappweb <admin@dappweb.cn>
2021-05-02 17:19:42 +0800

    fix bug

--- BNBecology.sol
+++ BNBecology.sol
@@ -1,5 +1,4 @@
-pragma solidity ^0.6.6;
-
+pragma solidity ^0.7.6;
 
 library SafeMath {
     /**
@@ -229,9 +228,6 @@ interface IBEP20 {
 }
 
 contract Context {
-    // Empty internal constructor, to prevent people from mistakenly deploying
-    // an instance of this contract, which should be used via inheritance.
-    constructor () internal { }
 
     function _msgSender() internal view returns (address payable) {
         return msg.sender;
@@ -251,7 +247,7 @@ contract Ownable is Context {
     /**
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
-    constructor () internal {
+    constructor () {
         address msgSender = _msgSender();
         _owner = msgSender;
         emit OwnershipTransferred(address(0), msgSender);
@@ -303,6 +299,7 @@ contract Ownable is Context {
 }
 
 contract BEP20Token is Context, IBEP20, Ownable {
+    
     using SafeMath for uint256;
 
     mapping (address => uint256) private _balances;
@@ -314,11 +311,14 @@ contract BEP20Token is Context, IBEP20, Ownable {
     string private _symbol;
     string private _name;
 
-    constructor( string memory name, string memory symbol, uint256  totalSupply) public {
-        _name = name ;
-        _symbol = symbol  ; 
+    constructor( 
+        string memory cname, 
+        string memory csymbol, 
+        uint256 ctotalSupply) {
+        _name = cname ;
+        _symbol = csymbol  ; 
         _decimals = 18;
-        _totalSupply =  totalSupply * uint256(10**18);
+        _totalSupply =  ctotalSupply * uint256(10**18);
         _balances[msg.sender] = _totalSupply;
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
@@ -555,7 +555,52 @@ contract BEP20Token is Context, IBEP20, Ownable {
     }
 }
 
-contract BNBEcology {
+
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+ 
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+ 
+    uint256 private _status;
+ 
+    constructor () {
+        _status = _NOT_ENTERED;
+    }
+ 
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+ 
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+ 
+        _;
+ 
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+
+contract BNBEcology is ReentrancyGuard {
     
     using SafeMath for uint256;
 
@@ -606,7 +651,7 @@ contract BNBEcology {
     event LimitReached(address indexed addr, uint256 amount);
     event NewRewards(address indexed addr, uint256 amount);
 
-    constructor(address payable development_address) public {
+    constructor(address payable development_address) {
 
         owner = msg.sender;
         
@@ -650,10 +695,6 @@ contract BNBEcology {
         cycles.push(12e19);
         cycles.push(24e19);
         cycles.push(48e19);
-    }
-
-    fallback() external payable {
-        _deposit(msg.sender, msg.value);
     }
 
     function _setUpline(address _addr, address _upline) private {
@@ -841,7 +882,7 @@ contract BNBEcology {
 
     }
 
-    function _transferTokenBEB(address _addr,uint256 _amount)  private{
+    function _transferTokenBEB(address _addr,uint256 _amount)  nonReentrant()  private{
         require(_amount > 0, "BEB Less Zero");
         BEB_token.transferFrom(address(this), _addr, _amount);
         users[_addr].srewards = users[_addr].srewards.add( _amount);
@@ -854,7 +895,7 @@ contract BNBEcology {
         _deposit(msg.sender, msg.value);
     }
 
-    function withdraw() external {
+    function withdraw() nonReentrant() external{
         (uint256 to_payout, uint256 max_payout) = this.payoutOf(msg.sender);
         require(users[msg.sender].payouts < max_payout, "Full payouts");
 
@@ -916,9 +957,7 @@ contract BNBEcology {
         
         total_withdraw += to_payout;
 
-        //msg.sender.transfer(to_payout);
-
-        BEB_token.transferFrom(address(this), msg.sender, to_payout);
+        msg.sender.transfer(to_payout);
         
         emit Withdraw(msg.sender, to_payout);
 
